c2cd0314fce97f86f8adaf38e4b0b205c59837fa
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-07 03:17:45 -0800

    print uppercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+#check if c is a lowercase letter
+	if ord('a') <= ord('c') <= ord('z'):
+		return True
+	else:
+		return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+def uppercase(str):
+	for i in str:
+	if ord(i) >= 97 and ord(i) <= 122:
+
