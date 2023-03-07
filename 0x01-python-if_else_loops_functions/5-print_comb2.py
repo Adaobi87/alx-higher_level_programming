cd1801a03ad7098c1f96df2633692fe6a242c22d
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-07 03:50:41 -0800

    print combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-	i < 99:
-	print(f"{i:02d}, ", end="")
-	else:
-	print(f"{i:02d}\n")
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
