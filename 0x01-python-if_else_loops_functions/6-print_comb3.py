02d3ce248737af09a1c74ca414c0ac217b31e9cb
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-07 03:53:26 -0800

    print combination3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for n in range(0, 10):
-	for x in range(n + 1, 10):
-	if n == 8 and x == 9:
-		print(98)
-	else:
-		print('{}{}, '.format(n,x), end='')
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
