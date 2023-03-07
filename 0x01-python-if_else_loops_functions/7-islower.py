5b6b60c1e2867ff9c793b5ce7787e122453700ea
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-07 03:59:16 -0800

    check lower case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-#check if c is a lowercase letter
-	if ord('a') <= ord('c') <= ord('z'):
-		return True
-	else:
-		return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
