81658866c606a126cb47514280cfd3002740e17f
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-06 18:25:34 -0800

    concatenate edge

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
+str = str[32:46] + str[46:57] + str[92:96]+ str[:6]
 print(str)
