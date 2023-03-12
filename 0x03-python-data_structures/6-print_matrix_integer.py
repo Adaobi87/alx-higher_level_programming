89ff86a60a64d0ce082989f0c22f7e96deb85aeb
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-03-12 09:47:41 -0700

    print matrix integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
-            if column == row(-1):
+            if column == row[-1]:
                 print('{:d}'.format(column), end='')
-                else:
-                    print('{:d}'.format(column), end=' ')
-                    print()
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
