6037a140adcba7afd376261331b88dc71e8a47ec
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-04-03 01:12:22 -0700

    real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """initializing the rectangle class
+        Args:
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero"""
+            self.width = width
+            self.height = height
+
+    @property
+    def width(self):
+        """retrieves width attributes"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width attributes"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__qwidth = value
+
+    @property
+    def height(self):
+        """retrieves height attributes"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height attributes"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0)
+        else:
+            self.__height = value 
