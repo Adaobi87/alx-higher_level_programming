0ff8486eb36d15c2becc3024c65e59bb9bdb3591
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-04-12 09:20:07 -0700

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,9 +8,9 @@ class student:
     def __init__(self, first_name, last_name, age):
         """initializes a new student"""
         self.first_name = first_name
-        self.last_name = lastname
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieves a dictionary representation of student"""
+        """Gets a dictionary representation of student"""
         return self.__dict__
