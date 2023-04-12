5cf02205a320c9d4d0f16929d333e0d30b296da5
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-04-12 10:30:55 -0700

    log parsing

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class student"""
+"""This module defines a class Student"""
 
 
-class student:
-    """Represents a student"""
+class Student:
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initializes a new student"""
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Gets a dictionary representation of student"""
+        """Gets a dictionary representation of the Student"""
         return self.__dict__
