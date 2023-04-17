26189235f8d9a287838aac7e570afc129f8cbe8d
Adaobi Okoye <adaobiokoye87@gmail.com>
2023-04-17 12:45:10 -0700

    test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,5 +14,54 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
+    def test_id_zero(self):
+        b = Base(0)
+        self.assertEqual(0, b.id)
+
+    def test_id_negative(self):
+        b = Base(-5)
+        self.assertEqual(-5, b.id)
+
+    def test_id_string(self):
+        b = Base("John")
+        self.assertEqual("John", b.id)
+
+    def test_id_list(self):
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
+
+    def test_id_dict(self):
+        b = Base({"id": 99})
+        self.assertEqual({"id": 99}, b.id)
+
+    def test_id_tuple(self):
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
+
+    def test_to_json_type(self):
+        sq = Square(1)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
+
+    def test_to_json_value(self):
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+
+    def test_to_json_None(self):
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_Empty(self):
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
 if __name__ == "__main__":
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -79,7 +79,78 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             rect = Rectangle(1, 10, 32)
 
-    def test_
+    def test_y_string(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, 10, 20)
+
+    def test_area(self):
+        self.assertEqual(self.r.area(), 5 * 10)
+        rect = Rectangle(3, 9, 8, 8, 2)
+        self.assertEqual(rect.area(), 3 * 9)
+
+    def test_str_overload(self):
+        r = Rectangle(5, 10, 8, 7, 88)
+        self.assertEqual(r.__str__(), "[Rectangle] (88) 8/7 - 5/10")
+
+    def test_update_id(self):
+        self.r.update(54)
+        self.assertEqual(54, self.r.id)
+
+    def test_update_width(self):
+        self.r.update(54, 30)
+        self.assertEqual(30, self.r.width)
+
+    def test_update_height(self):
+        self.r.update(54, 30, 10)
+        self.assertEqual(10, self.r.height)
+
+    def test_update_x(self):
+        self.r.update(54, 30, 10, 6)
+        self.assertEqual(6, self.r.x)
+
+    def test_update_y(self):
+        self.r.update(54, 30, 10, 6, 2)
+        self.assertEqual(2, self.r.y)
+
+    def test_update_dict(self):
+        self.r.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(1, self.r.y)
+        self.assertEqual(2, self.r.width)
+        self.assertEqual(3, self.r.x)
+        self.assertEqual(89, self.r.id)
+
+    def test_update_dict_list(self):
+        self.r.update(1000, y=1, width=2, x=3, id=89)
+        self.assertEqual(1000, self.r.id)
+
+    def test_to_dict(self):
+        r1 = Rectangle(5, 4)
+        self.assertEqual(type(r1.to_dictionary()), dict)
+
+    def test_saving_to_file(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        r1 = Rectangle(5, 10, 0, 0, 346)
+        Rectangle.save_to_file([r1])
+
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+        t = [{"x": 0, "y": 0, "id": 346, "height": 10, "width": 5}]
+        self.assertEqual(t, json.loads(content))
+
+    def test_json_string(self):
+            list_input = [
+                {'id': 2089, 'width': 10, 'height': 4},
+                {'id': 2712, 'width': 1, 'height': 7}]
+            json_list_input = Rectangle.to_json_string(list_input)
+            list_output = Rectangle.from_json_string(json_list_input)
+            s1 = {'id': 2089, 'width': 10, 'height': 4}
+            s2 = {'height': 7, 'id': 2712, 'width': 1}
+            self.assertEqual(list_input[0], s1)
+            self.assertEqual(list_input[1], s2)
+
 
 if __name__ == "__main__":
     unittest.main()
